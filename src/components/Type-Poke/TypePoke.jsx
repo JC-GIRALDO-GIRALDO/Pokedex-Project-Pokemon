6b25ab9f4c003cfb3645e6a490ac2dd6a5752e7c
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-11-01 21:16:25 -0500

    se mejoro la funcionalidad de los botones para devolver a la galeria de pokemons

--- src/components/Type-Poke/TypePoke.jsx
+++ src/components/Type-Poke/TypePoke.jsx
@@ -18,23 +18,97 @@ const TypeImages = ({ types, onTypeClick }) => {
 
 // Componente para mostrar la lista de Pokémon
 const PokemonList = ({ selectedType, pokemonList, onBackClick }) => {
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [pokemonDetails, setPokemonDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchPokemonDetails = async () => {
+      if (selectedPokemon) {
+        try {
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`
+          );
+          if (!response.ok) {
+            throw new Error("Network response was not ok.");
+          }
+          const data = await response.json();
+          setPokemonDetails(data);
+        } catch (error) {
+          console.error("There was a problem with the fetch operation:", error);
+        }
+      }
+    };
+
+    fetchPokemonDetails();
+  }, [selectedPokemon]);
+
+  const handlePokemonClick = (pokemonName) => {
+    setSelectedPokemon(pokemonName);
+  };
+
+  const handleBackClick = () => {
+    setSelectedPokemon(null);
+    setPokemonDetails(null);
+  };
+
+  const showPokemonDetails = () => {
+    if (pokemonDetails) {
+      return (
+        <div>
+          <button onClick={handleBackClick}>Volver</button>
+          <h2>Detalles de {pokemonDetails.name}</h2>
+          <p>Altura: {pokemonDetails.height}</p>
+          <p>Peso: {pokemonDetails.weight}</p>
+          <h3>Habilidades:</h3>
+          <ul>
+            {pokemonDetails.abilities.map((ability, index) => (
+              <li key={index}>{ability.ability.name}</li>
+            ))}
+          </ul>
+          <h3>Tipo:</h3>
+          <ul>
+            {pokemonDetails.types.map((type, index) => (
+              <li key={index}>{type.type.name}</li>
+            ))}
+          </ul>
+          <h3>Imagen:</h3>
+          <img
+            src={pokemonDetails.sprites.front_default}
+            alt={pokemonDetails.name}
+          />
+          {/* Aquí podrías agregar detalles sobre evoluciones e información adicional */}
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div>
-      <button onClick={onBackClick}>Volver</button>
-      <h2>Pokémon de tipo {selectedType}:</h2>
-      <div className="pokemon-list">
-        {pokemonList.map((pokemon, index) => (
-          <div key={index}>
-            <p>{pokemon.pokemon.name}</p>
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                pokemon.pokemon.url.split("/")[6]
-              }.png`}
-              alt={pokemon.pokemon.name}
-            />
+      {selectedPokemon ? (
+        showPokemonDetails()
+      ) : (
+        <div>
+          <button onClick={onBackClick}>Volver</button>
+          <h2>Pokémon de tipo {selectedType}:</h2>
+          <div className="pokemon-list">
+            {pokemonList.map((pokemon, index) => (
+              <div
+                key={index}
+                onClick={() => handlePokemonClick(pokemon.pokemon.name)}
+              >
+                <p>{pokemon.pokemon.name}</p>
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    pokemon.pokemon.url.split("/")[6]
+                  }.png`}
+                  alt={pokemon.pokemon.name}
+                />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
@@ -84,11 +158,13 @@ export const TypePoke = () => {
 
   const handleTypeClick = (typeName) => {
     setSelectedType(typeName);
+    setShowPokemonList(false); // Ocultar la lista al cambiar de tipo
   };
 
   const handleBackClick = () => {
-    setSelectedType("");
-    setShowPokemonList(false);
+    setSelectedType(""); // Reiniciar la selección del tipo
+    setPokemonList([]); // Limpiar la lista de Pokémon
+    setShowPokemonList(false); // Ocultar la lista al volver atrás
   };
 
   return (
@@ -108,3 +184,5 @@ export const TypePoke = () => {
     </div>
   );
 };
+
+export default TypePoke;
